8c12f9c5c8b986099532a988db144bcae7f11049
Tau Africa Intl <jonuasa@gmail.com>
2025-10-18 22:12:31 +0300

    Implement service worker for caching assets

---
+++ Service-worker.js
@@ -0,0 +1,18 @@
+
+// service-worker.js
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      // Add other files you want to cache
+    ]))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
